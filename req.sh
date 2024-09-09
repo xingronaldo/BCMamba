77898bfc20d0e954e83f31a4225ed09ca80723a9
Guangxing Wang <87854190+xingronaldo@users.noreply.github.com>
2024-09-09 22:01:18 +0800

    Update req.sh

--- req.sh
+++ req.sh
@@ -6,7 +6,7 @@ pip install einops
 pip install kornia
 pip install timm==0.6.13
 pip install causal-conv1d==1.1.0
-pip install mmcv-full==1.6.0 -f https://download.openmmlab.com/mmcv/dist/cu116/torch1.12.1/index.html
+%pip install mmcv-full==1.6.0 -f https://download.openmmlab.com/mmcv/dist/cu116/torch1.12.1/index.html
 apt-get update -y
 apt-get install libglib2.0-0 -y
 
